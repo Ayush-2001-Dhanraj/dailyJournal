d235bee5acb9a38f4df4f7c077204ee72be4a2d1
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-01-10 20:56:00 +0530

    update content

--- app.js
+++ app.js
@@ -5,71 +5,67 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Greetings! I'm Ayush Dhanraj, a passionate Software Developer based in Lucknow, India. Currently pursuing my Master's in Computer Science and Engineering, specializing in AI and Data Science at MNNIT Allahabad. With a focus on mobile and app-based solutions, I thrive on transforming ideas into seamless digital experiences. My journey reflects a commitment to staying ahead in the dynamic tech landscape, where creativity meets innovation. Explore my projects and discover the intersection of technology and user-centric design that defines my approach to software development.";
+const aboutContent =
+  "Hello, I'm Ayush Dhanraj, a mobile and app-based Software Developer from Lucknow, India. Currently pursuing an MTech in AI and Data Science at MNNIT Allahabad, I'm dedicated to crafting innovative digital solutions. My passion lies in creating seamless user experiences through cutting-edge technology. Explore my journey and projects to witness the intersection of creativity and technology in my software development endeavors.";
+const contactContent =
+  "dhanrajaayush123@gmail.com | ayushdhanraj.work@gmail.com";
 
 const app = express();
 const posts = [];
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("home", {
     staringContent: homeStartingContent,
-    posts : posts
+    posts: posts,
   });
 });
 
-app.get("/posts/:post", function(req,res) {
+app.get("/posts/:post", function (req, res) {
   const nPost = lodash.lowerCase(req.params.post);
   posts.forEach((element) => {
     const cPost = lodash.lowerCase(element.title);
-    if(cPost === nPost) {
-      res.render("post",{newPost : element});
+    if (cPost === nPost) {
+      res.render("post", { newPost: element });
       console.log("Match!");
-    }else {
+    } else {
       console.log("Not a match!");
     }
   });
-
 });
 
-app.get("/about", function(req,res) {
-  res.render("about", {aboutContent : aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req,res) {
-  res.render("contact", {contactContent : contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res) {
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res) {
+app.post("/compose", function (req, res) {
   const newPost = {
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
   };
   posts.push(newPost);
   //  console.log(posts);
   res.redirect("/");
 });
 
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
